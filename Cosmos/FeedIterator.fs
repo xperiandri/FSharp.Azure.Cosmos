6c4d717089761425490579228e28acb41268994b
Andrii Chebukin <andrii@eCierge.com>
2025-01-11 19:28:57 +0400

    Implemented feed iterator pagination

---
+++ Cosmos/FeedIterator.fs
@@ -0,0 +1,54 @@
+﻿[<AutoOpen>]
+module Microsoft.Azure.Cosmos.FeedIteratorExtensions
+
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open System.Threading
+open Microsoft.Azure.Cosmos
+open FSharp.Control
+
+// See https://github.com/Azure/azure-cosmos-dotnet-v3/issues/903
+type FeedIterator<'T> with
+
+    /// Converts the iterator to an async sequence of items.
+    member iterator.AsAsyncEnumerable<'T> ([<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) = taskSeq {
+        while iterator.HasMoreResults do
+            let! page = iterator.ReadNextAsync (cancellationToken)
+
+            for item in page do
+                cancellationToken.ThrowIfCancellationRequested ()
+                yield item
+    }
+
+    /// Converts the iterator to an async sequence of items with continuation token.
+    member iterator.AsContinuableAsyncEnumerable<'T> ([<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) = taskSeq {
+        while iterator.HasMoreResults do
+            let! page = iterator.ReadNextAsync (cancellationToken)
+
+            for item in page do
+                cancellationToken.ThrowIfCancellationRequested ()
+                yield struct (item, page.ContinuationToken)
+    }
+
+    /// Converts the iterator to an async sequence of items with continuation token.
+    member iterator.AsContinuableAsyncEnumerable<'T> (desiredItemCount, [<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) = taskSeq {
+        let mutable itemsRead = 0
+        while iterator.HasMoreResults && itemsRead < desiredItemCount do
+            let! page = iterator.ReadNextAsync (cancellationToken)
+
+            for item in page do
+                cancellationToken.ThrowIfCancellationRequested ()
+                itemsRead <- itemsRead + 1
+                yield struct (item, page.ContinuationToken)
+    }
+
+    /// Converts the iterator to an async sequence of items with their ETag values.
+    member iterator.AsTaggedAsyncEnumerable<'T>  ([<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) = taskSeq {
+        while iterator.HasMoreResults do
+            let! page = iterator.ReadNextAsync (cancellationToken)
+
+            for item in page do
+                cancellationToken.ThrowIfCancellationRequested ()
+                yield struct (item, page.ETag)
+    }
+
--- Cosmos/TaskSeq.fs
+++ Cosmos/TaskSeq.fs
@@ -1,35 +1,9 @@
 ﻿module Microsoft.Azure.Cosmos.TaskSeq
 
 open System.Linq
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 open System.Threading
 open Microsoft.Azure.Cosmos
 open Microsoft.Azure.Cosmos.Linq
-open FSharp.Control
-
-// See https://github.com/Azure/azure-cosmos-dotnet-v3/issues/903
-type FeedIterator<'T> with
-
-    /// Converts the iterator to an async sequence of items.
-    member iterator.AsAsyncEnumerable<'T> ([<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) = taskSeq {
-        while iterator.HasMoreResults do
-            let! page = iterator.ReadNextAsync (cancellationToken)
-
-            for item in page do
-                cancellationToken.ThrowIfCancellationRequested ()
-                yield item
-    }
-
-    /// Converts the iterator to an async sequence of items with their ETag values.
-    member iterator.AsAsyncEnumerableWithTags<'T> ([<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) = taskSeq {
-        while iterator.HasMoreResults do
-            let! page = iterator.ReadNextAsync (cancellationToken)
-
-            for item in page do
-                cancellationToken.ThrowIfCancellationRequested ()
-                yield struct (item, page.ETag)
-    }
 
 /// <summary>
 /// Asynchronously iterates Cosmos DB <see cref="FeedIterator{T}" />.
@@ -49,7 +23,7 @@ let ofFeedIteratorWithCancellation<'T> (cancellationToken : CancellationToken) (
 /// Asynchronously iterates Cosmos DB <see cref="FeedIterator{T}" /> producing items with their ETag values.
 /// </summary>
 /// <param name="iterator">Cosmos DB feed iterator</param>
-let ofFeedIteratorWithETags<'T> (iterator : FeedIterator<'T>) = iterator.AsAsyncEnumerableWithTags<'T> ()
+let ofFeedIteratorWithETags<'T> (iterator : FeedIterator<'T>) = iterator.AsTaggedAsyncEnumerable<'T> ()
 
 /// <summary>
 /// Asynchronously iterates Cosmos DB <see cref="FeedIterator{T}" /> producing items with their ETag values.
@@ -57,7 +31,7 @@ let ofFeedIteratorWithETags<'T> (iterator : FeedIterator<'T>) = iterator.AsAsync
 /// <param name="cancellationToken">Cancellation token</param>
 /// <param name="iterator">Cosmos DB feed iterator</param>
 let ofFeedIteratorWithETagsAndCancellation<'T> (cancellationToken : CancellationToken) (iterator : FeedIterator<'T>) =
-    iterator.AsAsyncEnumerableWithTags<'T> (cancellationToken)
+    iterator.AsTaggedAsyncEnumerable<'T> (cancellationToken)
 
 /// <summary>
 /// Created Cosmos DB <see cref="FeedIterator{T}" /> from <see cref="IQueryable{T}" /> and asynchronously iterates it.
@@ -70,4 +44,5 @@ let ofCosmosDbQuerable<'T> (query : IQueryable<'T>) = query.ToFeedIterator().AsA
 /// </summary>
 /// <param name="query">Cosmos DB querable</param>
 /// <param name="cancellationToken">Cancellation token</param>
-let ofCosmosDbQuerableWithCancellation<'T> (cancellationToken : CancellationToken) (query : IQueryable<'T>) = query.ToFeedIterator().AsAsyncEnumerable<'T> (cancellationToken)
+let ofCosmosDbQuerableWithCancellation<'T> (cancellationToken : CancellationToken) (query : IQueryable<'T>) =
+    query.ToFeedIterator().AsAsyncEnumerable<'T> (cancellationToken)
